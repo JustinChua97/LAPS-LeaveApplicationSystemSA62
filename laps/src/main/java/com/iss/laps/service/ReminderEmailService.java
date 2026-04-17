4c550bb99efe90bf737ab5e0227bab46ecd39da2
Zhong Cheng <zctiong@u.nus.edu>
2026-04-17 11:13:52 +0800

    Revert "Refactored the leave templates to include a direct"
    This reverts commit feed8891dc218c08268781788cc483f2c332b455.

--- laps/src/main/java/com/iss/laps/service/ReminderEmailService.java
+++ laps/src/main/java/com/iss/laps/service/ReminderEmailService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
-import com.iss.laps.exception.MessageNotSentException;
 import com.iss.laps.model.LeaveApplication;
 
 import jakarta.mail.MessagingException;
@@ -47,12 +46,8 @@ public class ReminderEmailService {
             String body = buildBody(application, daysUntilStart);
 
             sendEmail(recipient, subject, body);
-        } catch (MessageNotSentException e) {
-            log.warn("Failed to send reminder email", e);
-            throw e;
         } catch (Exception e) {
-            log.error("Failed to send reminder email", e);
-            throw new MessageNotSentException("Failed to send reminder email: " + e.getMessage(), e);
+            log.warn("Failed to send reminder email", e);
         }
     }
 
@@ -70,8 +65,8 @@ public class ReminderEmailService {
         context.setVariable("endDate", application.getEndDate());
         context.setVariable("reason", application.getReason());
         context.setVariable("daysUntilStart", daysUntilStart);
-        String LeaveUrl = appHost + "/manager/leaves/" + application.getId();
-        context.setVariable("leaveUrl", LeaveUrl);
+        context.setVariable("appHost", appHost);
+
         // NEW dedicated template for reminders
         return templateEngine.process("emails/leave-reminder", context);
     }
